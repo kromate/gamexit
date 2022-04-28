8d0615ef24288e198164040af5033678b9f1b35b
kromate <akpananthony33@gmail.com>
2022-04-28 18:03:02 +0100

    feat: update

---
+++ functions/src/server.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+/* eslint-disable no-fallthrough */
+
+/**
+ * Module dependencies.
+ */
+
+import 'reflect-metadata'
+import app from './app'
+const debug = require('debug')('socketio-server:server')
+import * as http from 'http'
+// import socketServer from './socket'
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '9000')
+app.set('port', port)
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app)
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+// const io = socketServer(server)
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr!.port
+  debug('Listening on ' + bind)
+
+  console.log('Server Running on Port: ', port)
+}
---
+++ functions/src/socket.ts
@@ -0,0 +1,21 @@
+import { useSocketServer } from "socket-controllers";
+import { Server } from "socket.io";
+
+export default (httpServer) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  // io.on("connection", (socket) => {
+
+  // });
+
+  useSocketServer(io, { controllers: [__dirname + "/api/controllers/*.ts"] });
+
+  return io;
+};
+
+
+exports.widgets = functions.https.onRequest(app);
