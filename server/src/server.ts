f1ffb6b41ff9de475c5e9a63978de7201610e727
kromate <akpananthony33@gmail.com>
2022-04-29 02:37:12 +0100

    --- Auto-Git Commit ---

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
-import { Server } from 'socket.io'
+const express = require('express')
+const socketIO = require('socket.io')
+const cors = require('cors')
+const PORT = process.env.PORT || 9000
+const INDEX = '/index.html'
+const app = express();
 
-const io = new Server(9000, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
-  }
-})
+app.use(cors())
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const io = socketIO(server)
+
+// const io = new Server(9000, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST']
+//   }
+// })
 
 
 
@@ -18,4 +30,3 @@ io.on('connection', (socket) => {
 })
 
 
-// io.listen(9000)
