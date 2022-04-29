a35b79c1e1a8c11592cae2df5799967ad07ca1f0
kromate <akpananthony33@gmail.com>
2022-04-29 01:36:57 +0100

    --- Auto-Git Commit ---

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,89 +1,16 @@
-#!/usr/bin/env node
-/* eslint-disable no-fallthrough */
-
-/**
- * Module dependencies.
- */
-
-import 'reflect-metadata'
-import app from './app'
-const debug = require('debug')('socketio-server:server')
-import * as http from 'http'
-
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '9000')
-app.set('port', port)
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
   }
+})
+io.on('connection', (socket) => {
+  socket.emit('hello', 'youtube tutorial')
+  socket.on('play', (index) => {
+    console.log('server received', index)
+    socket.broadcast.emit('play', index)
+  })
+})
 
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
-
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
-    default:
-      throw error
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr!.port
-  debug('Listening on ' + bind)
-
-  console.log('Server Running on Port: ', port)
-}
+server.listen(9000)
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,22 +1,238 @@
 <template>
-	<div>Test</div>
+
+	<div class="container">
+		<h1>Tic tac toe</h1>
+		<div class="play-area">
+			<div id="block_0" class="block" @click="draw(0, false)">{{ content[0] }}</div>
+			<div id="block_1" class="block" @click="draw(1, false)">{{ content[1] }}</div>
+			<div id="block_2" class="block" @click="draw(2, false)">{{ content[2] }}</div>
+			<div id="block_3" class="block" @click="draw(3, false)">{{ content[3] }}</div>
+			<div id="block_4" class="block" @click="draw(4, false)">{{ content[4] }}</div>
+			<div id="block_5" class="block" @click="draw(5, false)">{{ content[5] }}</div>
+			<div id="block_6" class="block" @click="draw(6, false)">{{ content[6] }}</div>
+			<div id="block_7" class="block" @click="draw(7, false)">{{ content[7] }}</div>
+			<div id="block_8" class="block" @click="draw(8, false)">{{ content[8] }}</div>
+		</div>
+		<h2 id="winner" v-if="isOver"> Winner is {{winner}} </h2>
+		<h2 v-if="isTie"> Game is Tie</h2>
+		<button @click="resetBoard()" v-if="isOver || isTie">RESET BOARD</button>
+
+	</div>
 </template>
 
-<script lang="ts" setup>
-import {onMounted} from 'vue'
-import {io} from 'socket.io-client'
+<script>
+import io from 'socket.io-client'
+const socket = io('http://localhost:9000')
+export default {
+	name: 'App',
+	components: {
+	},
+	data() {
+		return {
+			content: ['', '', '', '', '', '', '', '', ''],
+			turn: true,
+			isOver: false,
+			winner: null,
+			isTie: false
+		}
+	},
+	methods: {
+		draw(index, drawFromOther) {
+			// send event to socket.io
+			if(this.turn) {
+				// if turn is true then mark as X or mark as O
+				this.content[index] = 'X'
+			} else {
+				this.content[index] = 'O'
+			}
+			if (!drawFromOther) {
+				socket.emit('play', index)
+			}
 
-  const connectSocket = async () => {
-    const socket = await socketService
-      .connect("http://localhost:9000")
-      .catch((err) => {
-        console.log("Error: ", err);
-      });
-  };
+			this.turn = !this.turn
+			this.calculateWinner()
+			this.calculateTie()
+		},
+		calculateWinner() {
+			const WIN_CONDITIONS = [
+				// rows
+				[0, 1, 2], [3, 4, 5], [6, 7, 8],
+				// cols
+				[0, 3, 6], [1, 4, 7], [2, 5, 8],
 
-onMounted(connectSocket)
+				// diagonals
+
+				[0, 4, 8], [2, 4, 6]
+
+			]
+			for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+				const firstIndex = WIN_CONDITIONS[i][0]
+				const secondIndex = WIN_CONDITIONS[i][1]
+				const thirdIndex = WIN_CONDITIONS[i][2]
+				if(this.content[firstIndex] == this.content[secondIndex] &&
+          this.content[firstIndex] == this.content[thirdIndex] &&
+        this.content[firstIndex] != '') {
+					this.isOver = true
+					this.winner = this.content[firstIndex]
+				}
+			}
+		},
+		calculateTie(){
+			for (let i = 0 ; i<= 8 ; i++) {
+				if(this.content[i] == '') {
+					return
+				}
+			}
+			this.isTie = true
+		},
+		resetBoard() {
+			for (let i=0; i<= 8; i++) {
+				this.content[i] = ''
+				this.isOver = false
+				this.winner = null
+				this.isTie = false
+			}
+		}
+	},
+	created() {
+		socket.on('hello', (msg)=>{
+			console.log(msg)
+		})
+		socket.on('play', (index) => {
+			console.log('received index', index)
+			this.draw(index, true)
+		})
+	}
+}
 </script>
 
-<style  scoped>
+<style>
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  .container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #eee;
+  }
+
+  h1 {
+    font-size: 5rem;
+    margin-bottom: 0.5em;
+  }
+
+  h2 {
+    margin-top: 1em;
+    font-size: 2rem;
+    margin-bottom: 0.5em;
+  }
+
+  .play-area {
+    display: grid;
+    width: 300px;
+    height: 300px;
+    grid-template-columns: auto auto auto;
+  }
+
+  .block {
+    display: flex;
+    width: 100px;
+    height: 100px;
+    align-items: center;
+    justify-content: center;
+    font-size: 3rem;
+    font-weight: bold;
+    border: 3px solid black;
+    transition: background 0.2s ease-in-out;
+  }
+
+  .block:hover {
+    cursor: pointer;
+    background: #0ff30f;
+  }
+
+  .occupied:hover {
+    background: #ff3a3a;
+  }
+
+  .win {
+    background: #0ff30f;
+  }
+
+  .win:hover {
+    background: #0ff30f;
+  }
+
+  #block_0,
+  #block_1,
+  #block_2 {
+    border-top: none;
+  }
+
+  #block_0,
+  #block_3,
+  #block_6 {
+    border-left: none;
+  }
+
+  #block_6,
+  #block_7,
+  #block_8 {
+    border-bottom: none;
+  }
+
+  #block_2,
+  #block_5,
+  #block_8 {
+    border-right: none;
+  }
+
+  button {
+    outline: none;
+    border: 4px solid green;
+    padding: 10px 20px;
+    font-size: 1rem;
+    font-weight: bold;
+    background: none;
+    transition: all 0.2s ease-in-out;
+  }
+
+  button:hover {
+    cursor: pointer;
+    background: green;
+    color: white;
+  }
+
+  .playerWin {
+    color: green;
+  }
+
+  .computerWin {
+    color: red;
+  }
+
+  .draw {
+    color: orangered;
+  }
+
+  @media only screen and (max-width: 600px) {
+
+    h1 {
+      font-size: 3rem;
+      margin-bottom: 0.5em;
+    }
+
+    h2 {
+      margin-top: 1em;
+      font-size: 1.3rem;
+    }
+  }
 </style>
