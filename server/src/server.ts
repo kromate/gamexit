31a1f38a191386a920a13a188d9cb9f9376392e1
kromate <akpananthony33@gmail.com>
2022-04-29 01:52:28 +0100

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,21 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server, {
+import { Server } from 'socket.io'
+
+const io = new Server(9000, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
   }
 })
+
+
+
+
 io.on('connection', (socket) => {
-  socket.emit('hello', 'youtube tutorial')
   socket.on('play', (index) => {
     console.log('server received', index)
     socket.broadcast.emit('play', index)
   })
 })
 
-server.listen(9000)
+
+// io.listen(9000)
