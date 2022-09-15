17f645a6888302d29b5f2e8d118d3866ef53ba78
kromate <akpananthony33@gmail.com>
2022-09-16 00:05:05 +0100

    chore: migrating  styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,14 @@ module.exports = {
     './src/pages/**/*.vue'
   ],
   theme: {
-   		extend: {
+    extend: {
 			colors: {
 				primary: 'var(--primary)',
-        secondary: 'var(--secondary)',
-        white: 'var(--white)',
-				dark_blue: 'var(--dark_blue)',
-				grey: 'var(--grey)',
-				light_grey: 'var(--light_grey)',
-				purple: 'var(--purple)'
+        dark: 'var(--dark)',
+        blue: 'var(--blue)'
+      },
+      boxShadow: {
+				block: '0 20px rgba(0,0,0,0.8)'
 			}
 		}
   },
